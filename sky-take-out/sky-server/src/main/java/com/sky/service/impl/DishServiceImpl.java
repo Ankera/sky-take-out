dd8414a4b9444f37487b319a63d0f0e644325a5a
yu <222@qq.com>
2026-06-03 01:06:41 +0800

    feat: add 菜品增删改查

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -1,11 +1,16 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
+import com.sky.result.PageResult;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,6 +42,54 @@ public class DishServiceImpl implements DishService {
         if (flavors != null && !flavors.isEmpty()) {
             flavors.forEach(dishFlavor -> {
                 dishFlavor.setDishId(id);
+            });
+
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+
+    @Override
+    public PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO) {
+        PageHelper.startPage(dishPageQueryDTO.getPage(), dishPageQueryDTO.getPageSize());
+        Page<DishVO> dishVOS = dishMapper.pageQuery(dishPageQueryDTO);
+        return new PageResult(dishVOS.getTotal(), dishVOS.getResult());
+    }
+
+    @Override
+    public void deleteBatch(List<Long> ids) {
+        for (Long id : ids) {
+            dishMapper.deleteById(id);
+        }
+    }
+
+    @Override
+    public DishVO getByIdWithFlavor(Long id) {
+
+        DishVO dishVO = new DishVO();
+
+        Dish dish = dishMapper.getById(id);
+        BeanUtils.copyProperties(dish, dishVO);
+
+        List<DishFlavor> dishFlavors = dishFlavorMapper.getByDishId(dishVO.getId());
+        dishVO.setFlavors(dishFlavors);
+
+        return dishVO;
+    }
+
+    @Override
+    public void updateWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+        dishMapper.update(dish);
+
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (flavors != null) {
+            // 先删除老的
+            dishFlavorMapper.deleteByDishId(dishDTO.getId());
+
+            // 再更新
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(dishDTO.getId());
             });
 
             dishFlavorMapper.insertBatch(flavors);
